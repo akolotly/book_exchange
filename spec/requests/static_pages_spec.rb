8ce0a9b934b186a6a6216ab63367f19ab7b3a564
Anastasiya Kolotova <parvus.bombus@gmail.com>
2018-04-10 17:13:06 +0300

    About us pages (#1)
    * update database.yml
    
    * Add about us page
    
    * Fix code style
    
    * Add rubocop
    
    * Fix minor errors

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Static pages' do
+  subject { page }
+
+  describe 'About us' do
+    before { visit about_us_path }
+
+    it 'Displays the correct content' do
+      is_expected.to have_selector('h1', text: 'О нас')
+      is_expected.to have_title('About us')
+    end
+  end
+end
