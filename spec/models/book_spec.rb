b67138da3b7016f3f45a436b9f3a1b090c667beb
Anastasiya <parvus.bombus@gmail.com>
2018-04-28 22:41:27 +0300

    Add Book model

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Book, type: :model do
+  subject { Book.new(title: 'Приключения Алисы в Стране чудес') }
+
+  describe '#valid?' do
+    context 'When all attributes is present' do
+      it 'answer methods' do
+        is_expected.to respond_to(:title)
+      end
+    end
+  end
+
+  it { is_expected.to be_valid }
+
+  describe 'Validation' do
+    context 'When title is empty' do
+      it 'not be valid' do
+        subject.title = ''
+        is_expected.not_to be_valid
+      end
+    end
+  end
+end
