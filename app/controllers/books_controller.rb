7fb9444f6c757083ce29c47436dad8afee6be4bf
Anastasiya Kolotova <parvus.bombus@gmail.com>
2018-05-16 19:12:07 +0300

    Modeling books (#4)
    * Add brakeman & bundle-audit
    
    * Add Book model
    
    * Finish book create, new, edit, update, index, and destroy actions
    
    * Fix code style

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  before_action :find_book, only: %i[edit update destroy show]
+
+  def index
+    @books = Book.all
+  end
+
+  def show; end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = 'Книга добавлена в каталог!'
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @book.update_attributes(book_params)
+      flash[:success] = 'Книга отредактирована!'
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book.destroy!
+    flash[:success] = 'Книга удалена'
+    redirect_to books_url
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :book do
+    title 'Алиса в зазеркалье'
+  end
+end
